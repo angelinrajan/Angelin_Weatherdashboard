864adf77228e9cdb68cb02608955fe2d54066c25
AR <angelinrajan20@gmail.com>
2023-07-08 18:32:17 -0400

    adding additional code for 5 day forecast

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,11 @@ var inputCity = document.querySelector('#userCity');
 var currentWeather = document.querySelector('.card');
 var Submit = document.querySelector('#submitBtn');
 var listsearch = document.querySelector('.recentlySearched');
-var cname = document.querySelector('.cityName');
+var cname = document.querySelector('.cityNameDate');
+var ctemp = document.querySelector('.cityTemp');
+var cwind = document.querySelector('.cityWind');
+var chumidity = document.querySelector('.cityHumidity');
+var fiveDayInfo = document.querySelector('.fiveDayblock');
 var cityforStoring = '';
 var cityCoord;
 /*
@@ -52,7 +56,19 @@ var getUserCity = function (city) {
                         console.log(data);
                         var today = data.list[0];
                         var todayDate = dayjs.unix(today.dt).format("M/D/YYYY");
-                        cname.setAttribute("value", data.city.name);
+                        var cityn = data.city.name;
+                        cname.textContent = cityn + ":  " + todayDate;
+                        console.log(cname)
+                        var tempc = data.list[0].main.temp;
+                        ctemp.textContent = "Temp : " + tempc;
+                        console.log(ctemp)
+                        var windc = data.list[0].wind.speed;
+                        cwind.textContent = "Wind : " + windc;
+                        console.log(cwind)
+                        var humidityc = data.list[0].main.humidity;
+                        chumidity.textContent = "Humidity : " + humidityc;
+                        console.log(chumidity)
+                        //cdate.textContent = todayDate;
                         console.log(todayDate);
                         console.log(today);
                         for (var i = 7; i < data.list.length; i += 8) {
@@ -60,7 +76,28 @@ var getUserCity = function (city) {
                             var fiveDate = dayjs.unix(day.dt).format("M/D/YYYY");
                             console.log(fiveDate);
                             console.log(day);
+
+                            var daySelection = document.createElement("div");
+                            daySelection.setAttribute("class", "daysInfo");
+                            var dateSection = document.createElement("h3");
+                            // var iconSection = document.createElement("img")
+                            var temperatureSection = document.createElement("p")
+                            var windSection = document.createElement("p")
+                            var humiditySection = document.createElement("p")
+
+                            dateSection.textContent = fiveDate;
+                            temperatureSection.textContent = "Temp : " + day.main.temp;
+                            windSection.textContent = "Wind : " + day.wind.speed;
+                            humiditySection.textContent = "Humidity : " + day.main.humidity;
+
+                            daySelection.appendChild(dateSection);
+                            daySelection.appendChild(temperatureSection);
+                            daySelection.appendChild(windSection);
+                            daySelection.appendChild(humiditySection);
+                            console.log(daySelection.innerText)
+                            fiveDayInfo.appendChild(daySelection);
                         };
+                        console.log(fiveDate)
                     });
             } else {
                 alert('Error: ' + response.statusText);
@@ -83,7 +120,7 @@ function getRecentSearch() {
 
             eachSearch.innerText = citystore[i];
             eachSearch.setAttribute("class", "btnCity");
-            eachSearch.addEventListener("click", function(event){
+            eachSearch.addEventListener("click", function (event) {
                 var city = event.target.innerText;
                 getUserCity(city);
             })
